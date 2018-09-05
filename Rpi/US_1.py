060b4a416a2b56c96cfaad87441b036b92c35217
KimJihwan <eric967712@ajou.ac.kr>
2018-09-05 22:06:00 +0900

    rpi

---
+++ Rpi/US_1.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as gpio
+import time
+  
+gpio.setmode(gpio.BCM)
+
+trig = 17
+echo = 18
+
+print "start"
+
+fp = open("us_1.txt",'w')
+
+gpio.setup(trig, gpio.OUT)
+gpio.setup(echo, gpio.IN)
+
+
+try :
+    while True :
+        gpio.output(trig, False)
+        time.sleep(0.5)
+
+        gpio.output(trig, True)
+        time.sleep(0.00001)
+        gpio.output(trig, False)
+
+        while gpio.input(echo) == 0 :
+            pulse_start = time.time()
+
+        while gpio.input(echo) == 1 :
+            pulse_end = time.time()
+
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17000
+        distance = round(distance, 2)
+        
+        fp.write(str(distance))
+        print "Distance : ", distance, "cm"
+
+
+except :
+    gpio.cleanup()
